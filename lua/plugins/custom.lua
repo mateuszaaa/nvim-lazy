07583950827b2d5a858e2524ddd580133188d6c4
Mateusz Nowakowski <mat13mn@gmail.com>
2023-10-09 22:02:04 +0200

    github link & multi cursors

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,7 +1,30 @@
 return {
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        'smoka7/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+            {
+                mode = { 'v', 'n' },
+                '<Leader>m',
+                '<cmd>MCunderCursor<cr>',
+                desc = 'Create a selection for selected text or word under the cursor',
+            },
+        },
+  },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "tpope/vim-fugitive" },
+  {
+    "vincent178/nvim-github-linker",
+    config = function()
+        require("nvim-github-linker").setup()
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
     opts = {
